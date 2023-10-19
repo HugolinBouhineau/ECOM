42035608247b19528ddf073897494b6511ec9ca6
romainn <noe.romain@etu.univ-grenoble-alpes.fr>
2023-10-19 15:26:00 +0200

    html et backend fonctionnent -> manque que "add to cart"

--- src/main/webapp/app/components/product/product.component.ts
+++ src/main/webapp/app/components/product/product.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Query} from '@angular/core';
+import {PlantService} from "../../entities/plant/service/plant.service";
+import {ActivatedRoute, Router, Params} from '@angular/router';
+import {IPlant} from "../../entities/plant/plant.model";
 
 @Component({
   selector: 'jhi-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class ProductComponent implements OnInit {
+  plant:any;
+  href:string = "";
+  stock:number = 0;
+  plantID:number = 0;
+  imagePath:string = "";
+  path:string = "";
+  constructor(private ps:PlantService, private activatedRoute:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.plantID = params['PlantId'];});
+
+    this.ps.find(this.plantID).subscribe(
+      plant => {
+        this.plant = plant.body;
+        console.log(plant)
+        this.stock = this.plant.stock;
+        this.imagePath = this.path + "/" + this.plant.imagePath;
+
+      }
+    );
+  }
 }
