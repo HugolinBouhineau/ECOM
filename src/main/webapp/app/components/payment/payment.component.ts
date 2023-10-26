0065b184f555fe63412fd0c4d1e381f8a9a0fc54
Pierre-Mar <pierre.marjolin@gmail.com>
2023-10-26 16:38:27 +0200

    Get Address of a customer (not working for front)

--- src/main/webapp/app/components/payment/payment.component.ts
+++ src/main/webapp/app/components/payment/payment.component.ts
@@ -4,6 +4,8 @@ import { AccountService } from '../../core/auth/account.service';
 import { CustomerService } from '../../entities/customer/service/customer.service';
 import { ICustomer } from '../../entities/customer/customer.model';
 import { Account } from 'app/core/auth/account.model';
+import {AddressService} from "../../entities/address/service/address.service";
+import {IAddress} from "../../entities/address/address.model";
 
 @Component({
   selector: 'jhi-payment',
@@ -13,8 +15,13 @@ import { Account } from 'app/core/auth/account.model';
 export class PaymentComponent implements OnInit {
   authenticatedUser: Account | undefined;
   customer: ICustomer | undefined;
+  addresses: IAddress[] | undefined;
 
-  constructor(private accountService: AccountService, private customerService: CustomerService) {}
+  constructor(
+    private accountService: AccountService,
+    private customerService: CustomerService,
+    private addressService: AddressService
+  ) {}
 
   ngOnInit(): void {
     this.accountService.identity().subscribe((user: Account | null) => {
@@ -26,7 +33,16 @@ export class PaymentComponent implements OnInit {
     });
     this.customerService.getCurrentCustomer().subscribe((customer: ICustomer | undefined) => {
       this.customer = customer
-    })
+    });
+    if (this.customer != undefined) {
+      this.addressService.findByCustomerId(this.customer.id).subscribe((addresses: IAddress[] | null) => {
+        if (addresses != null) {
+          this.addresses = addresses;
+        } else {
+          this.addresses = undefined;
+        }
+      })
+    }
   }
 
   submit() {}
