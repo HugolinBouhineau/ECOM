383d5a340a352991d67dbf457c5b83c9dbcc1e1f
romainn <noe.romain@etu.univ-grenoble-alpes.fr>
2023-11-09 13:14:26 +0100

    ajout d'une boite de dialogue pour l'annulation de commande

---
+++ src/main/webapp/app/command-dialog/command-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector:'jhi-command-dialog',
+  templateUrl: './command-dialog.component.html',
+  styleUrls: ['./command-dialog.component.scss'],
+})
+export class CommandDialogComponent implements OnInit {
+
+  @Input() title: string | undefined;
+  @Input() message: string | undefined;
+  @Input() btnOkText: string | undefined;
+  @Input() btnCancelText: string | undefined;
+
+  constructor(private activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+  public decline() {
+    this.activeModal.close(false);
+  }
+
+  public accept() {
+    this.activeModal.close(true);
+  }
+
+  public dismiss() {
+    this.activeModal.dismiss();
+  }
+
+}
