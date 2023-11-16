99bb94c4c45fea01886de229daa0e55aac6f89ea
romainn <noe.romain@etu.univ-grenoble-alpes.fr>
2023-11-16 11:34:52 +0100

    correction de bug pour le HOME

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,6 +1,4 @@
 import { Route } from '@angular/router';
-
-import {CatalogComponent} from "../components/catalog/catalog.component";
 import {HomeComponent} from "./home.component";
 
 export const HOME_ROUTE: Route = {
