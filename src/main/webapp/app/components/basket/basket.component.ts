d2d5080f1d7b5e3b693deba9e2c0b4f6b194af6e
Hugolin Bouhineau-Chevigny <hugolin.bouhineau-chevigny@etu.univ-grenoble-alpes.fr>
2023-11-26 15:46:39 +0100

    fix eslint error

--- src/main/webapp/app/components/basket/basket.component.ts
+++ src/main/webapp/app/components/basket/basket.component.ts
@@ -86,7 +86,6 @@ export class BasketComponent implements OnInit {
   }
 
   getImageUrl(item: Item): string {
-    console.log('je suis une erreur eslint');
     if (item.plant.imagePath) {
       return item.plant.imagePath.split('**')[0];
     } else {
